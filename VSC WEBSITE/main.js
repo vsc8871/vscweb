23839734bff3a6ea8b50d8bec4a9d3b29ccecbd2
VIKRAM SINGH CAHUHAN <158603173+vsc8871@users.noreply.github.com>
2024-08-27 20:45:43 +0530

    Add files via upload

---
+++ VSC WEBSITE/main.js
@@ -0,0 +1,56 @@
+ // navbar menu show option
+ const menuicon = document.querySelector(".mobile-toggle-menu");
+ const navbar = document.querySelector(".menu-wrap");
+ menuicon.addEventListener("click", () => {
+     navbar.classList.toggle("show-hide");
+ });
+
+ // darkmode option on\off function
+ const body = document.querySelector("body");
+ const darkmode = document.querySelector(".darkmode-toggle");
+ darkmode.addEventListener("click", () => {
+     body.classList.toggle("dark");
+ });
+
+ // type efect function 
+ const typedTextSpan = document.querySelector(".typed-text");
+ const cursorSpan = document.querySelector(".cursor");
+
+ const textArray = ["Welcome To Vikram Programing", "Created By Vikram Singh Chuahan", "A Programer", "A Web Developer", "An Engineer"];
+ const typingDelay = 100;
+ const erasingDelay = 50;
+ const newTextDelay = 2000; // Delay between current and next text
+ let textArrayIndex = 0;
+ let charIndex = 0;
+
+ function type() {
+     if (charIndex < textArray[textArrayIndex].length) {
+         if (!cursorSpan.classList.contains("typing")) cursorSpan.classList.add("typing");
+         typedTextSpan.textContent += textArray[textArrayIndex].charAt(charIndex);
+         charIndex++;
+         setTimeout(type, typingDelay);
+     }
+     else {
+         cursorSpan.classList.remove("typing");
+         setTimeout(erase, newTextDelay);
+     }
+ }
+
+ function erase() {
+     if (charIndex > 0) {
+         if (!cursorSpan.classList.contains("typing")) cursorSpan.classList.add("typing");
+         typedTextSpan.textContent = textArray[textArrayIndex].substring(0, charIndex - 1);
+         charIndex--;
+         setTimeout(erase, erasingDelay);
+     }
+     else {
+         cursorSpan.classList.remove("typing");
+         textArrayIndex++;
+         if (textArrayIndex >= textArray.length) textArrayIndex = 0;
+         setTimeout(type, typingDelay + 1100);
+     }
+ }
+
+ document.addEventListener("DOMContentLoaded", function () { // On DOM Load initiate the effect
+     if (textArray.length) setTimeout(type, newTextDelay + 250);
+ });
